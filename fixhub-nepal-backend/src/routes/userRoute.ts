b0ad5bcecdeb9fb43d366c288f0a5fa4f7fe0060
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-03-09 00:28:35 +0545

    backend: update user authentication routes

--- fixhub-nepal-backend/src/routes/userRoute.ts
+++ fixhub-nepal-backend/src/routes/userRoute.ts
@@ -1,11 +1,28 @@
 import { Router } from "express";
-import { registerUser, loginUser, sendResetLink, resetPassword } from "../controllers/userController";
+import {
+  registerUser,
+  loginUser,
+  sendResetLink,
+  resetPassword,
+  changePassword,
+  sendResetOtp,
+  resetPasswordWithOtp,
+} from "../controllers/userController";
+import { authenticateUser } from "../middlewares/authorizedUser";
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Web: link-based reset
 router.post("/forgot-password", sendResetLink);
 router.post("/reset-password/:token", resetPassword);
+
+// Mobile: OTP-based reset
+router.post("/forgot-password-otp", sendResetOtp);
+router.post("/reset-password-otp", resetPasswordWithOtp);
+
+router.post("/change-password", authenticateUser as any, changePassword as any);
 
 export default router;
