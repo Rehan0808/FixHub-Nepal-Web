0addc93da0e4f51d112ad6b4919dafc3700cfe30
Rehan Pradhan <rehanpradhan10@gmail.com>
2026-03-09 00:28:24 +0545

    backend: update review API routes

--- fixhub-nepal-backend/src/routes/reviewRoute.ts
+++ fixhub-nepal-backend/src/routes/reviewRoute.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createServiceReview } from "../controllers/reviewController";
+import { createServiceReview, getAllReviews } from "../controllers/reviewController";
 import { authenticateUser } from "../middlewares/authorizedUser";
 
 const router = Router();
 
+router.get("/", authenticateUser as any, getAllReviews);
 router.route("/:bookingId").post(authenticateUser as any, createServiceReview as any);
 
 export default router;
